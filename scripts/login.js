9b8fa83dc7e006ed0c041a084568138a7aff9570
joypark-inkorea <joy.heejungpark@gmail.com>
2025-09-28 23:35:21 +0900

    Update login.js
    Signed-off-by: joypark-inkorea <joy.heejungpark@gmail.com>

--- scripts/login.js
+++ scripts/login.js
@@ -1,16 +1,15 @@
-// ************* 여길 채워주세요! *************
-// Firebase 콘솔에서 확인한 내 프로젝트의 설정 정보를 붙여넣으세요.
+// ************* 중요!! *************
+// Firebase 콘솔에서 확인한 내 프로젝트의 설정 정보를 여기에 붙여넣으세요.
 const firebaseConfig = {
-  apiKey: "AIzaSyDA0BNmhnr37KqyI7oj766TwB8FrejsRzo",
+apiKey: "AIzaSyDA0BNmhnr37KqyI7oj766TwB8FrejsRzo",
   authDomain: "my-inventory-final.firebaseapp.com",
   projectId: "my-inventory-final",
   storageBucket: "my-inventory-final.firebasestorage.app",
   messagingSenderId: "740246970535",
   appId: "1:740246970535:web:f7738b92a6097671f67b82",
   measurementId: "G-4ZF63VWX6Z"
-
 };
-// ********************************************
+// **********************************
 
 // Firebase 앱 초기화
 firebase.initializeApp(firebaseConfig);
@@ -21,7 +20,7 @@ const emailInput = document.getElementById('email');
 const passwordInput = document.getElementById('password');
 const loginError = document.getElementById('login-error');
 
-// 로그인 상태 감지: 이미 로그인 되어있으면 메인 페이지로 이동
+// 로그인 상태 감지: 이미 로그인 되어있으면 메인 페이지로 자동 이동
 auth.onAuthStateChanged(user => {
     if (user) {
         window.location.href = 'index.html';
@@ -62,5 +61,3 @@ loginForm.addEventListener('submit', (e) => {
             }
         });
 });
-
-

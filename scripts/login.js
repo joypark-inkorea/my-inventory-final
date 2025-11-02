69ce1f039d1fea98811167e7dfaba3cb3e37041c
joypark-inkorea <joy.heejungpark@gmail.com>
2025-11-02 11:56:28 +0900

    Refactor Firebase login logic and session handling
    Updated Firebase configuration and added session persistence with auto logout after 2 hours.
    
    Signed-off-by: joypark-inkorea <joy.heejungpark@gmail.com>

--- scripts/login.js
+++ scripts/login.js
@@ -1,7 +1,7 @@
 // ************* 중요!! *************
 // Firebase 콘솔에서 확인한 내 프로젝트의 설정 정보를 여기에 붙여넣으세요.
 const firebaseConfig = {
-  apiKey: "AIzaSyAx0BJw2CNX4-LdJR8M7wZAgBIrFZhMzQg",
+  apiKey: "AIzaSyDNFkJW7BPLV2OyaFQl7OEM4kq8wHuej3Y",
   authDomain: "my-inventory-final.firebaseapp.com",
   projectId: "my-inventory-final",
   storageBucket: "my-inventory-final.firebasestorage.app",
@@ -77,5 +77,6 @@ loginForm.addEventListener('submit', (e) => {
             }
         });
 });
+
 
 

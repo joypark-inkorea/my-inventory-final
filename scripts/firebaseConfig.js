c11c6f418a409a1eb3f650922c54e5c71a91716b
joypark-inkorea <joy.heejungpark@gmail.com>
2025-11-02 12:16:37 +0900

    Add Firebase configuration to firebaseConfig.js
    Added Firebase configuration settings for the project.
    
    Signed-off-by: joypark-inkorea <joy.heejungpark@gmail.com>

--- scripts/firebaseConfig.js
+++ scripts/firebaseConfig.js
@@ -1,1 +1,12 @@
-
+// ************* 중요!! *************
+// Firebase 콘솔에서 확인한 내 프로젝트의 설정 정보를 여기에 붙여넣으세요.
+const firebaseConfig = {
+ apiKey: "AIzaSyDNFkJW7BPLV2OyaFQl7OEM4kq8wHuej3Y",
+  authDomain: "my-inventory-final.firebaseapp.com",
+  projectId: "my-inventory-final",
+  storageBucket: "my-inventory-final.firebasestorage.app",
+  messagingSenderId: "740246970535",
+  appId: "1:740246970535:web:f7738b92a6097671f67b82",
+  measurementId: "G-4ZF63VWX6Z"
+  
+};
